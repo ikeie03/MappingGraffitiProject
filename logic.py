21c09afaba7483f520d9059a6e5af1f5f261a27e
Isaac Exposito <fortiscadere03@gmail.com>
2024-02-05 23:18:53 -0600

    added docstrings to python functions

--- logic.py
+++ logic.py
@@ -1,3 +1,19 @@
+"""
+Module: logic
+Description: This module provides functions to extract and transform GPS coordinates from image files.
+
+Dependencies:
+- pprint
+- PIL (Python Imaging Library)
+- pillow_heif
+- piexif
+
+Usage:
+1. Import the module: `import logic`
+2. Use functions like `logic.get_x_coordinate(file)` and `logic.get_y_coordinate(file)` to extract GPS coordinates.
+
+"""
+
 from pprint import pprint
 from PIL import Image
 from pillow_heif import register_heif_opener
@@ -7,6 +23,15 @@ import pprint
 codec = 'ISO-8859-1'
 
 def get_x_coordinate(file):
+    """
+    Extracts and returns the X-coordinate (longitude) from the given image file.
+
+    Parameters:
+    - file (str): Path to the image file.
+
+    Returns:
+    - float: The Y-coordinate (longitude) in decimal degrees.
+    """
     register_heif_opener()
     image = Image.open(file)
 
@@ -22,6 +47,15 @@ def get_x_coordinate(file):
     return x_value
 
 def get_y_coordinate(file):
+    """
+    Extracts and returns the Y-coordinate (latitude) from the given image file.
+
+    Parameters:
+    - file (str): Path to the image file.
+
+    Returns:
+    - float: The Y-coordinate (latitude) in decimal degrees.
+    """
     register_heif_opener()
     image = Image.open(file)
 
@@ -29,14 +63,21 @@ def get_y_coordinate(file):
     pprint.pprint(exif_dict)
     gps_dict = exif_transform(exif_dict)
 
-    print('HERE:', gps_dict['GPSLatitude'])
-
     x_value = (convert_to_decimal_degrees(gps_dict['GPSLatitude'], 
                                         gps_dict['GPSLatitudeRef']))
     
     return x_value
 
 def exif_transform(exif_dict):
+    """
+    Helper function for the coordinates functions; reformats the data to a readable python dictionary.
+
+    Parameters:
+    - exif_dict (dict): Exif dictionary containing GPS information.
+
+    Returns:
+    - dict: Transformed GPS dictionary.
+    """
     gps_dict = {}
 
     gps_dict['GPS'] = {}
